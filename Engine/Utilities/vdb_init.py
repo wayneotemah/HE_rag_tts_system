e1405dcf74c9705878e880a0843693310aea20dc
wayne <wayneotemahegesa@gmail.com>
2024-04-16 11:20:56 +0300

    Removed: persistance storage

--- Engine/Utilities/vdb_init.py
+++ Engine/Utilities/vdb_init.py
@@ -24,61 +24,36 @@ class DocSeachService:
         Initialize the database with the documents in the RAG_docs folder
         """
         self.file_path = os.path.join(os.getcwd(),"media","RAG_docs")
-        if not os.path.exists(v_db_path):
-            #check if the vector store directory exists
-            #if it doen't create it and inintaliase the database
-            print("Initializing the database...")
-            os.mkdir(v_db_path)
+        print("Initializing the database...")
         
-            try:
-                
-                documents = self.load_data(self.file_path)
-                    
-                # splitting the text into
-                child_splitter  = RecursiveCharacterTextSplitter(chunk_size=1000)
-
-                self.vectorstore = Chroma(
-                    collection_name="manifesto",
-                    embedding_function=OpenAIEmbeddings(openai_api_key=os.getenv("openai_api_key","")),
-                    persist_directory=v_db_path,
-                )
-                store = InMemoryStore()
-                
-                self.retriever = ParentDocumentRetriever(
-                    vectorstore=self.vectorstore,
-                    docstore=store,
-                    child_splitter=child_splitter,
-                    parent_splitter=None,
-                )
-                self.retriever.add_documents(documents)
-
-                # persiste the db to disk
-                self.vectorstore.persist()
-            except Exception as e:
-                logging.exception(f"An error occurred while initializing the database:\n{e}")
-                raise e
-        else:
-            #if the vector store directory exists load the database
-            print("Loading the database...")
-            try:
-                documents = self.load_data(self.file_path)
-                
-                self.vectorstore = Chroma(
-                    collection_name="manifesto",
-                    embedding_function=OpenAIEmbeddings(openai_api_key=os.getenv("openai_api_key","")),
-                    persist_directory=v_db_path,
-                )
-                store = InMemoryStore()
-                self.retriever = ParentDocumentRetriever(
-                    vectorstore=self.vectorstore,
-                    docstore=store,
-                    child_splitter=RecursiveCharacterTextSplitter(chunk_size=1000),
-                )
-                self.retriever.add_documents(documents)
-            except Exception as e:
-                logging.exception(f"An error occurred while loading the database:\n{e}")
-                raise e
+        try:
             
+            documents = self.load_data(self.file_path)
+                
+            # splitting the text into
+            child_splitter  = RecursiveCharacterTextSplitter(chunk_size=1000)
+
+            self.vectorstore = Chroma(
+                collection_name="manifesto",
+                embedding_function=OpenAIEmbeddings(openai_api_key=os.getenv("openai_api_key","")),
+                # persist_directory=v_db_path,
+            )
+            store = InMemoryStore()
+            
+            self.retriever = ParentDocumentRetriever(
+                vectorstore=self.vectorstore,
+                docstore=store,
+                child_splitter=child_splitter,
+                parent_splitter=None,
+            )
+            self.retriever.add_documents(documents)
+
+            # persiste the db to disk
+            self.vectorstore.persist()
+        except Exception as e:
+            logging.exception(f"An error occurred while initializing the database:\n{e}")
+            raise e
+
     def load_data(self,file_path):
 
         # Create a PyPDFLoader
